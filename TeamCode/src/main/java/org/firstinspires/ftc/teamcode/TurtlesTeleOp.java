83f1cfb9895c718e03d02146d1faa9ec928bf187
PurpleLaptop2 <techturtles10179@gmail.com>
2019-10-19 12:07:50 -0500

    WheelController moveturn and used in TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtlesTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtlesTeleOp.java
@@ -3,13 +3,17 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public class TurtlesTeleOp extends OpMode {
+    WheelController wheelController;
+
     @Override
     public void init() {
+        wheelController = new WheelController(hardwareMap);
 
     }
 
     @Override
     public void loop() {
+        wheelController.moveXYTurn(gamepad1.right_stick_x, gamepad1.right_stick_y, gamepad1.left_stick_x);
 
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelController.java
@@ -69,6 +69,30 @@ public class WheelController {
         backRight.setPower(-speed);
     }
 
+    public void moveXYTurn(double tx, double ty, double tspeed) {
+        double x = -Range.clip(tx, -1, 1);
+        double y = Range.clip(ty, -1, 1);
+        double speed = Range.clip(tspeed, -1, 1);
+
+        // The speed at which we will move the robot
+        double r = Math.hypot(-x, y);
+
+        // The angle at which the will move the robot
+        double robotAngle = Math.atan2(y, -x) - Math.PI / 4;
+
+        // Do the calculations for the wheel speeds
+        double v1 = r * Math.cos(robotAngle);
+        double v2 = r * Math.sin(robotAngle);
+        double v3 = r * Math.sin(robotAngle);
+        double v4 = r * Math.cos(robotAngle);
+
+        // Finally, take the math we did for the speed of the wheels and actually set the wheel's speed
+        frontLeft.setPower(-v1 + speed);
+        frontRight.setPower(-v2 - speed);
+        backLeft.setPower(-v3 + speed);
+        backRight.setPower(-v4 - speed);
+    }
+
     public void stopWheels() {
         moveXY(0,0);
     }
