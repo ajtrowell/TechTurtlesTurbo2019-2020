bf0080486409e96f097293a2d686a0a33dfe8e68
PurpleLaptop1 <techturtles10179@gmail.com>
2019-10-19 12:10:24 -0500

    Made version 1 of autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimedAutoFull.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimedAutoFull.java
@@ -1,6 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
+@Autonomous(name="Autonomous")
 public class TimedAutoFull extends LinearOpMode {
+    public void runOpMode(){
+    WheelController wheelController = new WheelController(hardwareMap);
+    Servo arm;
+    arm = hardwareMap.servo.get("arm");
+
+    //moves forward to foundation
+        wheelController.moveXY(0,1);
+        sleep(4000);
+        wheelController.stopWheels();
+
+        //lifts arm and then drops it to grab foundation
+        arm.setPosition(0);
+        arm.setPosition(1);
+
+        //pushes foundation into the corner
+        wheelController.moveXY(0,1);
+        sleep(2000);
+        wheelController.stopWheels();
+
+        //releases foundation
+        arm.setPosition(0);
+
+        //moves backward under the skybridge
+        wheelController.moveXY(0,-1);
+        sleep(3500);
+        wheelController.stopWheels();
+    }
 }
