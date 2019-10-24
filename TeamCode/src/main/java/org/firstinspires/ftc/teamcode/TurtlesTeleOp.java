9e9ff4f1267625e05b41f389c1a14d35ea4e8fb8
PurpleLaptop2 <techturtles10179@gmail.com>
2019-10-24 17:16:10 -0500

    added other servo to teleop and renamed grabber to claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtlesTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurtlesTeleOp.java
@@ -7,13 +7,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class TurtlesTeleOp extends OpMode {
     WheelController wheelController;
     DcMotor armMotor;
-    Servo grabber;
+    Servo claw;
+    Servo foundationGrabber;
 
     @Override
     public void init() {
         wheelController = new WheelController(hardwareMap);
         armMotor = hardwareMap.dcMotor.get("armMotor");
-        grabber = hardwareMap.servo.get("grabber");
+        claw = hardwareMap.servo.get("claw");
+        foundationGrabber = hardwareMap.servo.get("foundationGrabber");
     }
 
     @Override
@@ -21,7 +23,9 @@ public class TurtlesTeleOp extends OpMode {
         wheelController.moveXYTurn(gamepad1.right_stick_x, gamepad1.right_stick_y, gamepad1.left_stick_x);
         armMotor.setPower(gamepad2.right_stick_y);
 
-        if (gamepad2.a) grabber.setPosition(1);
-        if (gamepad2.b) grabber.setPosition(0);
+        if (gamepad2.a) claw.setPosition(1);
+        if (gamepad2.b) claw.setPosition(0);
+        if (gamepad2.a) foundationGrabber.setPosition(1);
+        if (gamepad2.b) foundationGrabber.setPosition(0);
     }
 }
