42b58631610e7e19ed82b99f7f325cc06f881a8f
PurpleLaptop2 <techturtles10179@gmail.com>
2019-10-31 18:03:32 -0500

    made debug teleop an actual opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DebugTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DebugTeleOp.java
@@ -1,9 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.lib.Configurator;
 import org.firstinspires.ftc.teamcode.lib.WheelController;
+
+@TeleOp(name="Debug", group="default")
 
 public class DebugTeleOp extends OpMode {
     Configurator config;
