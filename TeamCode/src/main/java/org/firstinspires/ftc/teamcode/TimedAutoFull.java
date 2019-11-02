fc05e315f0382d3f408c1e5a4415bc99cc1b0d23
PurpleLaptop1 <techturtles10179@gmail.com>
2019-11-02 13:48:13 -0500

    changed arm to foundationGrabber to match new config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimedAutoFull.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TimedAutoFull.java
@@ -15,29 +15,29 @@ public class TimedAutoFull extends LinearOpMode {
     public void runOpMode(){
         config = new Configurator(this);
         wheelController = new WheelController(config);
-        Servo arm;
-        arm = hardwareMap.servo.get("arm");
+        Servo foundationGrabber;
+        foundationGrabber = hardwareMap.servo.get("foundationGrabber");
 
         //moves forward to foundation
-        wheelController.moveXY(0,1);
-        sleep(4000);
-        wheelController.stopWheels();
-
-        //lifts arm and then drops it to grab foundation
-        arm.setPosition(0);
-        arm.setPosition(1);
-
-        //pushes foundation into the corner
-        wheelController.moveXY(0,1);
+        wheelController.moveXY(0,-0.25);
         sleep(2000);
         wheelController.stopWheels();
 
+        //lifts arm and then drops it to grab foundation
+        foundationGrabber.setPosition(0);
+        foundationGrabber.setPosition(1);
+
+        //pushes foundation into the corner
+        wheelController.moveXY(0,0.25);
+        sleep(1000);
+        wheelController.stopWheels();
+
         //releases foundation
-        arm.setPosition(0);
+        foundationGrabber.setPosition(0);
 
         //moves backward under the skybridge
-        wheelController.moveXY(0,-1);
-        sleep(3500);
+        wheelController.moveXY(0,0.25);
+        sleep(875);
         wheelController.stopWheels();
     }
 }
